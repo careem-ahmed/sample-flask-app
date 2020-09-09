2dcffeb0b4cb433fc20a94b0ddf511f69eb8e843
Careem Ahamed <cahamed@thoughtworks.com>
2020-09-09 23:03:25 +0800

    adding dotenv

--- web/echo/config.py
+++ web/echo/config.py
@@ -4,9 +4,9 @@ import os
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = "changeme"
 DEBUG = os.environ.get('DEBUG', False)
-DB_NAME = "postgres"
-DB_USER = "postgres"
-DB_PASS = "postgres"
+DB_NAME = os.environ.get('DB_NAME')
+DB_USER = os.environ.get('DB_USER')
+DB_PASS = os.environ.get('DB_PASS')
 DB_SERVICE = "postgres"
 DB_PORT = "5432"
 SQLALCHEMY_DATABASE_URI = 'postgresql://{0}:{1}@{2}:{3}/{4}'.format(
